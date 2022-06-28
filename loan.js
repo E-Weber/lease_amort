51efcd79cda91b668775f08f8cb1eb847788de77
Eric Weber <eweber@entegracapital.com>
2022-06-28 14:00:57 -0500

    payoff

--- loan.js
+++ loan.js
@@ -56,6 +56,51 @@ function validate(){
     }
 }
 
+function payoff(){
+
+    var price = document.lease_form.price.value;
+    var down_payment = document.lease_form.down_payment.value;
+    var months = document.lease_form.months.value;
+    var rate = document.lease_form.rate.value;
+    var residual = document.lease_form.residual.value;
+    var broker_points = document.lease_form.broker_points.value;
+    
+ // isNaN(number()) checks to see if the user entered a float 
+                            
+    if (price <= 0 || isNaN(Number(price)) ) {
+        alert("Please enter a valid sell price.");
+        document.lease_form.price.value = "";
+    }
+    
+    else if (down_payment <= 0 || isNaN(Number(down_payment)) ) {
+        alert("Please enter a down payment.");
+        document.lease_form.down_payment.value = "";
+    }
+    else if (months <= 0 || parseInt(months) != months ) {
+        alert("Please enter a valid number of months.");
+        document.lease_form.months.value = "";
+    }
+                        
+    else if (rate <= 0 || isNaN(Number(rate))) {
+        alert("Please enter a valid interest rate.");
+        document.lease_form.rate.value = "";
+    }
+
+    else if (residual <= 0 || isNaN(Number(residual))) {
+        alert("Please enter a valid residual.");
+        document.lease_form.residual.value = "";
+    }
+ 
+    else if (broker_points <= 0 || isNaN(Number(broker_points))) {
+        alert("Please enter a valid broker commission.");
+        document.lease_form.broker_points.value = "";
+    }
+    
+    else {
+        payoff12(parseFloat(price), parseFloat(down_payment), parseInt(months), parseFloat(rate),  parseFloat(residual), parseFloat(broker_points));
+    }
+}
+
 function calculate(price, down_payment, months, rate, residual, broker_points) {
     
     i = rate/100;
@@ -182,7 +227,7 @@ function round(num, dec) {
     return(Math.round(num*Math.pow(10,dec))/ Math.pow(10,dec)).toFixed(dec);
 }
 
-function payoff12(){
+function payoff12(price, down_payment, months, rate, residual, broker_points){
     i = rate/100;
     residual = residual/100;
     
@@ -223,7 +268,7 @@ function payoff12(){
     document.getElementById("lease_info").innerHTML = info; //info is a string containing all html table code
 
     //-----------------------------------------------------------------------------
-
+    
     var table = "";
     table += "<table cellpadding='15' border=<'1'>";
     table += "<tr>";
@@ -241,21 +286,29 @@ function payoff12(){
     
     // pmt = pmt + early_payoff;
     
-    while(payment_counter < 13){
+    while(payment_counter < 14){
         //create rows
-
+        
         towards_interest = (i/12)*current_balance //calculates the portion of monthly payment that goes towards interest
         
-        if(pmt > current_balance) {
+        if(payment_counter < 13) {
             pmt = current_balance + towards_interest;
+            
         }
-
-        
 
         principle = pmt - towards_interest;
         total_interest = total_interest + towards_interest;
         current_balance = current_balance - principle;
 
+        if(payment_counter == 13){
+            var payment_counter = 13
+            var pmt = balance + residual_value + towards_interest;
+            var principle = 0;
+            var towards_interest = 0;
+            var total_interest = 0;
+            var current_balance = balance + residual_value + towards_interest;
+            continue
+        }
 
         //display row
         table+= "<tr>";
@@ -272,27 +325,23 @@ function payoff12(){
         
     }
 
-    while(payment_counter == 13){
+    // while(payment_counter == 13){
 
-        if(payment_counter == 13){
-            var pmt = balance + residual_value + towards_interest;
-            var principle = 0;
-            var towards_interest = 0;
-            var total_interest = 0;
-            var current_balance = residual_value;
+    //     if(payment_counter == 14){
+    //         continue;
 
-        }
+    //     }
 
-        table+= "<tr>";
-        table += "<td>"+payment_counter+"</td>";
-        table += "<td>"+round(pmt,2)+"</td>";
-        table += "<td>"+round(principle,2)+"</td>";
-        table += "<td>"+round(towards_interest,2)+"</td>";
-        table += "<td>"+round(total_interest,2)+"</td>";
-        table += "<td>"+round(current_balance,2)+"</td>";
-    table+= "</tr>";
+    //     table+= "<tr>";
+    //     table += "<td>"+payment_counter+"</td>";
+    //     table += "<td>"+round(pmt,2)+"</td>";
+    //     table += "<td>"+round(principle,2)+"</td>";
+    //     table += "<td>"+round(towards_interest,2)+"</td>";
+    //     table += "<td>"+round(total_interest,2)+"</td>";
+    //     table += "<td>"+round(current_balance,2)+"</td>";
+    // table+= "</tr>";
 
-    }
+    // }
 
     table += "</table>";
 
